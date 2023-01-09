dd1ae3278f449dda9c65ee3c0a9d27842dd05da2
Joe Helle <25853389+dievus@users.noreply.github.com>
2023-01-09 13:08:15 -0500

    Update msLDAPDump
    Now greps out the text document to look for interesting object descriptions.

--- msLDAPDump.py
+++ msLDAPDump.py
@@ -24,6 +24,7 @@ def banner():
 
 
 def ldap_search():
+    global domain
     try:
         username = input('Username (Y/N): ')
         if username == 'y' or username == 'Y':
@@ -41,14 +42,16 @@ def ldap_search():
                     sock_host = socket.gethostbyaddr(dom_con)
                     hostname = sock_host[0]
                     if hostname is not None:
-                        print(success + '[success] Target hostname is ' + hostname + '\n' + close)
+                        print(
+                            success + '[success] Target hostname is ' + hostname + '\n' + close)
 
                     else:
                         print(
                             info + '[info] Could not identify target hostname. Continuing...\n' + close)
                         hostname = dom_con
                 except Exception:
-                    print(fail + '[error] Unable to contact target host. Quitting...\n' + close)
+                    print(
+                        fail + '[error] Unable to contact target host. Quitting...\n' + close)
                     quit()
                 server = Server(dom_con, get_info=ALL)
                 conn = Connection(server, auto_bind=True)
@@ -60,38 +63,51 @@ def ldap_search():
                 print('-'*80)
                 time.sleep(2)
                 print(server.info)
+                info_server = str(server.info)
+                with open(f"{hostname}.ldapdump.txt", 'a') as f:
+                    f.write(info_server)
+                    f.close()
                 conn.unbind()
                 print(
-                    '[info] All set for now. Come back with credentials to dump additional domain information.\n')
+                    info + f'[info] All set for now. Come back with credentials to dump additional domain information. Information saved as {hostname}.ldapdump.txt\n' + close)
                 quit()
         elif username == 'n' or username == 'N':
-            print(info + '\n[info] No credentials, no problem. Binding to LDAP anonymously. Anonymous bind only allows dumping of basic information.' + close)
+            print(
+                info + '\n[info] No credentials, no problem. Binding to LDAP anonymously. Anonymous bind only allows dumping of basic information.' + close)
             dom_con = input('\nDomain Controller IP: ')
             server = Server(dom_con, get_info=ALL)
             conn = Connection(server, auto_bind=True)
-            print(info + '\n[info] Trying to identify the domain controller hostname...\n' + close)
+            print(
+                info + '\n[info] Trying to identify the domain controller hostname...\n' + close)
             try:
                 sock_host = socket.gethostbyaddr(dom_con)
                 hostname = sock_host[0]
                 if hostname is not None:
-                    print(success + '[success] Target hostname is ' + hostname + '\n' + close)
+                    print(
+                        success + '[success] Target hostname is ' + hostname + '\n' + close)
 
                 else:
                     print(
                         info + '[info] Could not identify target hostname. Continuing...\n' + close)
                     hostname = dom_con
             except Exception:
-                print(fail + '[error] Unable to contact target host. Quitting...\n' + close)
+                print(
+                    fail + '[error] Unable to contact target host. Quitting...\n' + close)
                 quit()
             print('-'*80)
-            print(info + '\n[info] Printing out a bunch of information on the target domain. Find Naming Contexts and enter the domain name from it.\n')
+            print(
+                info + '\n[info] Printing out a bunch of information on the target domain. Find Naming Contexts and enter the domain name from it.\n')
             print('[info] This will be needed later when authenticating, and is good information to have for enumeration.\n' + close)
             print('-'*80)
             time.sleep(2)
             print(server.info)
+            info_server = str(server.info)
+            with open(f"{hostname}.ldapdump.txt", 'a') as f:
+                f.write(info_server)
+                f.close()
             conn.unbind()
             print(
-                info + '[info] All set for now. Come back with credentials to dump additional domain information.\n' + close)
+                info + f'[info] All set for now. Come back with credentials to dump additional domain information. Information saved as {hostname}.ldapdump.txt\n' + close)
             quit()
         else:
             print(
@@ -102,8 +118,9 @@ def ldap_search():
         if know_dom == 'Y' or know_dom == 'y':
             domain = input('Domain name: ')
         elif know_dom == 'N' or know_dom == 'n':
-            print(info + '\n[info] Printing out a bunch of information on the target domain. Find Naming Contexts and enter the domain name from it.\n')
-            print('[info] This will be needed later when authenticating, and is good information to have for enumeration.\n' + close)
+            print(
+                info + "\n[info] Let's try to find the domain name. You can locate it under Naming contexts.\n" + close)
+            # print('[info] This will be needed later when authenticating, and is good information to have for enumeration.\n' + close)
             time.sleep(2)
             server = Server(dom_con, get_info=ALL)
             conn = Connection(server, auto_bind=True)
@@ -113,21 +130,26 @@ def ldap_search():
         domain_contents = domain.split(".")
         domain = domain_contents[0]
         dom_1 = f"DC={domain_contents[0]},DC={domain_contents[1]}"
-        print(info + '\n[info] Trying to identify the domain controller hostname...\n' + close)
+        print(
+            info + '\n[info] Trying to identify the domain controller hostname...\n' + close)
         try:
             sock_host = socket.gethostbyaddr(dom_con)
             hostname = sock_host[0]
             if hostname is not None:
-                print(success + '[success] Target hostname is ' + hostname + '\n' + close)
+                print(
+                    success + '[success] Target hostname is ' + hostname + '\n' + close)
 
             else:
-                print(info + '[info] Could not identify target hostname. Continuing...\n' + close)
+                print(
+                    info + '[info] Could not identify target hostname. Continuing...\n' + close)
                 hostname = dom_con
         except Exception:
-            print(fail + '[error] Unable to contact target host. Quitting...\n' + close)
+            print(
+                fail + '[error] Unable to contact target host. Quitting...\n' + close)
             quit()
         if username == 'n' or username == 'N' or password == 'n' or password == 'N':
-            print(info + '[info] End of the line. Once you have credentials you can authenticate and dump additional information.\n' + close)
+            print(
+                info + '[info] End of the line. Once you have credentials you can authenticate and dump additional information.\n' + close)
             quit()
         print(info + "[info] Let's dump some LDAP info...\n" + close)
         server = Server(hostname, get_info=ALL)
@@ -175,11 +197,45 @@ def ldap_search():
         time.sleep(2)
         print(
             success + f'\n[success] Information dump completed. A text file named {domain}.ldapdump.txt has been placed in this directory for your review.\n' + close)
+        time.sleep(2)
+        find_fields()
     except KeyboardInterrupt:
         print(
             info + '\n\n[info] You either fat fingered this or something else. Either way, quitting...\n' + close)
-    # except Exception as e:
-    #     print(e)
+    except Exception as e:
+        print(fail + f'[error] - {e}. Quitting...\n')
+
+
+def find_fields():
+    descript_info = []
+    idx = 0
+    print(
+        info + '[info] Checking the output for information in description fields.\n' + close)
+    with open(f'{domain}.ldapdump.txt', 'r') as refile:
+        lines = refile.readlines()
+        for line in lines:
+            descriptions = "description:"
+            if descriptions in line:
+                if "Built-in" in line or "Key Distribution Center Service Account" in line:
+                    pass
+                else:
+                    line = line.replace('description: ', '')
+                    descript_info.insert(idx, line)
+                    idx += 1
+            # descriptions = re.search("Description:", refile)
+    refile.close()
+    if len(descript_info) == 0:
+        print(info + '[info] No information identified based on static parameters. Check the output file manually. Quitting...' + close)
+        quit()
+    else:
+        lineLen = len(descript_info)
+        print(
+            success + '[success] Dumped the following information from object descriptions - Check text file for full details\n' + close)
+        for i in range(lineLen):
+            end = descript_info[i]
+            print(info + f'{end}')
+        print()
+        quit()
 
 
 if __name__ == "__main__":
